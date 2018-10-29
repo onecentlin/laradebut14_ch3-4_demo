7d07bf6da1a78efe9d5952dc794a4df56a5a3832
Winnie Lin <onecentlin@gmail.com>
2018-10-29 19:24:09 +0800

    3.1: 擷取出讀取檔案系統的類別，並呼叫它的方法

---
+++ src/FileExtensionManager.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Winnie\LaraDebut;
+
+class FileExtensionManager
+{
+    public function isValid(string $fileName): bool
+    {
+        if ($fileName === "") {
+            throw new \Exception("filename has to be provided");
+        }
+
+        if (strcasecmp(substr($fileName, -4), ".SLF") !== 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
--- src/LogAnalyzer.php
+++ src/LogAnalyzer.php
@@ -7,15 +7,7 @@ class LogAnalyzer
 {
     public function isValidLogFileName(string $fileName) : bool
     {
-        if ($fileName === "") {
-            throw new \Exception("filename has to be provided");
-        }
-
-        if (strcasecmp(substr($fileName, -4), ".SLF") !== 0) {
-            return false;
-        }
-
-        return true;
+        $mgr = new FileExtensionManager();
+        return $mgr->isValid($fileName);
     }
 }
-
